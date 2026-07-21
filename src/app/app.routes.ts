3a92a48c637586b1343ead655eb1c7fce093f106
SotoR713 <Soto_713@hotmail.com>
2026-07-20 17:48:17 -0700

    ligamiento de rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Activos } from './components/activos/activos';
+import { Empleados } from './components/empleados/empleados';
+import { Empresas } from './components/empresas/empresas';
+import { EntregarActivo } from './components/entregar-activo/entregar-activo';
+import { RecogerActivo } from './components/recoger-activo/recoger-activo';
+import { TiposActivo } from './components/tipos-activo/tipos-activo';
+
+export const routes: Routes = [
+  { path: 'activos', component: Activos },
+  { path: 'empleados', component: Empleados },
+  { path: 'empresas', component: Empresas },
+  { path: 'entregar-activo', component: EntregarActivo },
+  { path: 'recoger-activo', component: RecogerActivo },
+  { path: 'tipos-activo', component: TiposActivo },
+
+  // Redirección al abrir la aplicación
+  { path: '', redirectTo: 'activos', pathMatch: 'full' },
+];
